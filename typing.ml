f28615851c90643d3544871c7934d4ab3a16e9ef
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2019-02-18 23:38:04 +0100

    Add struct declaration and Eaccess_field and correct function declaration

--- typing.ml
+++ typing.ml
@@ -9,6 +9,8 @@ type context = (Ttree.ident, Ttree.typ) Hashtbl.t
 
 let (functions: (Ttree.ident, Ttree.typ * Ttree.typ list) Hashtbl.t) = Hashtbl.create 255
 
+let (structs: (Ttree.ident, Ttree.structure) Hashtbl.t) = Hashtbl.create 255
+
 let string_of_loc (l:(Lexing.position * Lexing.position)) =
   let file_of_pos (pos:Lexing.position) = if (String.equal pos.pos_fname "") then "" else " file:" ^ pos.pos_fname in
   let line_of_pos (pos:Lexing.position) = " line:" ^ (string_of_int pos.pos_lnum) in
@@ -29,10 +31,12 @@ let string_of_type = function
   | Ttypenull  -> "typenull"
 
 let rec is_well_defined = function
-  | Tint | Tvoidstar | Ttypenull -> assert true
-  | Tstructp s -> assert false
+  | Ptree.Tint -> true
+  | Ptree.Tstructp s -> 
+    if Hashtbl.mem structs s.id then true
+    else false
 
-let eq_of_type t1 t2 =
+let eq_of_type (t1:Ttree.typ) (t2:Ttree.typ) =
   match (t1, t2) with
   | (Tint, Tint) -> true
   | _ -> assert false
@@ -50,8 +54,24 @@ let rec type_expr (ctx: context) (expr: Ptree.expr) =
         let id_name = cast_ident id in
         if Hashtbl.mem ctx id_name then
           {expr_typ = Hashtbl.find ctx id_name; expr_node = Ttree.Eaccess_local id_name}
-            else raise_expr_error "not yet implemented message"
-      | Larrow (e, id) -> assert false
+        else
+          raise (Error ((string_of_loc id.id_loc) ^ " Type Error: variable " ^ id_name ^ " is not defined"))
+      | Larrow (e, id) -> 
+        (* e is of type Evalue on struct call *)
+        let e_typed = type_expr ctx e in
+        let field_name = cast_ident id in
+        begin match e_typed.expr_typ with
+          | Tstructp s ->
+            if Hashtbl.mem s.str_fields field_name then
+              let f  = Hashtbl.find s.str_fields field_name in
+              {
+                expr_typ = f.field_typ;
+                expr_node = Ttree.Eaccess_field (e_typed, f)
+              }
+            else
+              raise (Error ((string_of_loc id.id_loc) ^ " Type Error: struct field " ^ field_name ^ " is not well defined"))
+          | _ -> raise (Error ((string_of_loc e.expr_loc) ^ " Type Error: expression is not a struct pointer"))
+        end
     end
   | Ptree.Eassign (lv, e) ->
     let e_typed = type_expr ctx e in
@@ -97,11 +117,22 @@ let rec type_expr (ctx: context) (expr: Ptree.expr) =
         {expr_typ = ret_type;expr_node = Ttree.Ecall (cast_ident id, e_list_typed)}
       else raise_expr_error "not yet implemented message1"
     else raise_expr_error ("function: " ^ (cast_ident id))
-  | _ -> assert false
+  | Ptree.Esizeof id ->
+    (* Not sure if id is the name of the struct of the variable*)
+    let name = cast_ident id in
+    if Hashtbl.mem structs name then
+      {expr_typ = Tint; expr_node = Ttree.Esizeof (Hashtbl.find structs name)}
+    else assert false
+
 
 let type_typ = function
   | Ptree.Tint -> Ttree.Tint
-  | _ -> assert false
+  | Ptree.Tstructp s ->
+    let id = cast_ident s in
+    if Hashtbl.mem structs id then
+      Ttree.Tstructp (Hashtbl.find structs id)
+    else
+    raise (Error ((string_of_loc s.id_loc) ^ " Type Error(decl_struct): struct " ^ id ^ " was not defined"))
 
 let type_decl_var (dv:Ptree.decl_var) =
   let (t, id) = dv in (type_typ t, cast_ident id)
@@ -117,13 +148,6 @@ let type_decl_var_list (local_ctx: context) (dvl: Ptree.decl_var list) =
   in
   List.iter add_if_unique decl_var_list_typed;
   decl_var_list_typed
-
-let add_function name ret_type formals =
-  if Hashtbl.mem functions name then
-    raise (Error "type error, not yet implemented message, unicity")
-  else
-    let formals_type = List.map (fun (t, id) -> t) formals in
-    Hashtbl.add functions name (ret_type, formals_type)
 
 let rec type_stmt (ctx: context) (ret_typ: Ttree.typ) (st: Ptree.stmt) =
   match st.stmt_node with
@@ -152,17 +176,22 @@ and type_block (ctx: context) (ret_typ: Ttree.typ) block =
   let sts_typed = List.map (type_stmt local_ctx ret_typ) sts in
   (decl_vars_typed, sts_typed)
 
+let add_function fun_name ret_type formals =
+  let fun_name_cast = cast_ident fun_name in
+  if Hashtbl.mem functions fun_name_cast then
+  raise (Error ((string_of_loc fun_name.id_loc) ^ " Type Error(decl_fun): function " ^ fun_name_cast ^ " was already declared"))
+  else
+    let formals_type = List.map (fun (t, id) -> t) formals in
+    Hashtbl.add functions fun_name_cast (ret_type, formals_type)
+
 (* Attention, was VERY sleepy, check fun_body*)
 (* Attention aux recursives *)
 let type_decl_fun (ctx: context) (df: Ptree.decl_fun) =
   let fun_typ_typed = type_typ df.fun_typ in
   let fun_name_cast = cast_ident df.fun_name in
-  if Hashtbl.mem ctx fun_name_cast then
-    raise (Error ((string_of_loc df.fun_name.id_loc) ^ " Type Error(decl_fun): function " ^ fun_name_cast ^ " was already declared"))
-  else Hashtbl.add ctx fun_name_cast fun_typ_typed;
   let local_ctx = Hashtbl.copy ctx in
   let fun_formals_typed = type_decl_var_list local_ctx df.fun_formals in
-  add_function fun_name_cast fun_typ_typed fun_formals_typed;
+  add_function df.fun_name fun_typ_typed fun_formals_typed;
   let fun_body_typed = type_block local_ctx fun_typ_typed df.fun_body in
   {
     fun_typ = fun_typ_typed;
@@ -171,15 +200,35 @@ let type_decl_fun (ctx: context) (df: Ptree.decl_fun) =
     fun_body = fun_body_typed;
   }
 
-let type_decl (ctx: context) = function
-  | Ptree.Dfun df -> type_decl_fun ctx df
-  | _ -> assert false (* not yet implemented *)
+let type_decl_struct ds =
+  let ((id:Ptree.ident), (dvl:Ptree.decl_var list)) = ds in
+  if Hashtbl.mem structs id.id then
+    raise (Error ((string_of_loc id.id_loc) ^ " Type Error(decl_struct): struct " ^ id.id ^ " was already declared"))
+  else
+    let (fields: (Ttree.ident, Ttree.field) Hashtbl.t) = Hashtbl.create 255 in
+    Hashtbl.add structs id.id {str_name = id.id; str_fields = fields};
+    let valid_field (t, id) =
+      if Hashtbl.mem fields (cast_ident id) then
+        raise (Error ((string_of_loc id.id_loc) ^ " Type Error(decl_struct): struct field " ^ id.id ^ " was already declared"))
+      else if is_well_defined t then
+        Hashtbl.add fields (cast_ident id) {field_name = cast_ident id; field_typ = type_typ t}
+      else
+        raise (Error ((string_of_loc id.id_loc) ^ " Type Error(decl_struct): struct field " ^ id.id ^ " is not well defined"))
+    in
+    List.iter valid_field dvl;
+    Hashtbl.replace structs id.id {str_name = id.id; str_fields = fields}
+
 
 (* Performs typing on file AST produce via parsing
    Ptree.file -> exn Ttree.file *)
 let rec type_file (ctx: context) = function
   | [] -> {funs = []}
-  | h::t -> let h_typed = type_decl ctx h in { funs = h_typed::((type_file ctx t).funs) }
+  | (Ptree.Dfun f)::t -> 
+    let f_typed = type_decl_fun ctx f in
+    { funs = f_typed::((type_file ctx t).funs) }
+  | (Ptree.Dstruct s)::t ->
+    type_decl_struct s;
+    type_file ctx t
 
 let program (p:Ptree.file) =
   let ctx = Hashtbl.create 255 in
