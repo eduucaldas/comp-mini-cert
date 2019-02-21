20cd61b704bccebaf3307a7873f8e660642c02a6
Gabriel Oliveira Martins <gabriel.oliveiragom@gmail.com>
2019-02-21 21:28:54 +0100

    Repeat instructions to ERTL

--- ertl.ml
+++ ertl.ml
@@ -2,8 +2,28 @@ open Ertltree
 
 let instr = function
   | Rtltree.Econst (n, r, l) ->
-      Econst (n, r, l)
-  | _ -> Ereturn
+      Ertltree.Econst (n, r, l)
+  | Rtltree.Eload (r1, n, r2, l) ->
+      Ertltree.Eload (r1, n, r2, l)
+  | Rtltree.Estore (r1, r2, n, l) ->
+      Ertltree.Estore (r1, r2, n, l)
+  | Rtltree.Emunop (op, r, l) ->
+      Ertltree.Emunop (op, r, l)
+  | Rtltree.Embinop (op, r1, r2, l) ->
+      begin match op with
+      (* TODO *)
+      | Mdiv -> Embinop (op, r1, r2, l)
+      | _ -> Embinop (op, r1, r2, l)
+      end
+  | Rtltree.Emubranch (b, r, l1, l2) ->
+      Ertltree.Emubranch (b, r, l1, l2)
+  | Rtltree.Embbranch (b, r1, r2, l1, l2) ->
+      Ertltree.Embbranch (b, r1, r2, l1, l2)
+  | Rtltree.Ecall (r, f, r_list, l) ->
+      (* TODO *)
+      Ertltree.Ecall (f, min (List.length r_list) 6, l)
+  | Rtltree.Egoto l ->
+      Ertltree.Egoto (l)
 
 let deffun (df:Rtltree.deffun) =
   {
