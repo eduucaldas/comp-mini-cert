7a3cc2422e48346ba0b89390306dc7b26cce8912
Eduardo Caldas <eduucaldas@gmail.com>
2019-02-25 23:18:02 +0100

    ertl: fun call, broke

--- ertl.ml
+++ ertl.ml
@@ -29,8 +29,28 @@ let instr = function
   | Rtltree.Embbranch (b, r1, r2, l1, l2) ->
       Ertltree.Embbranch (b, r1, r2, l1, l2)
   | Rtltree.Ecall (r, f, r_list, l) ->
-      (* TODO *)
-      Ertltree.Ecall (f, min (List.length r_list) 6, l)
+    let n_of_stack_params = max 0 (List.length r_list - 6) in
+    let l_pop =
+      if n_of_stack_params == 0 then l
+      else
+        generate (Emunop (Maddi(Int32.of_int (8 * n_of_stack_params)), Register.rsp, l))
+    in
+
+    let l_ret = generate (Embinop (Mmov, Register.result, r, l_pop)) in
+    let ertl_call = Ecall (f, min (List.length r_list) 6, l_ret) in
+
+    let rec pass_args i r_l ertl =
+        match r_l with
+        | [] -> ertl
+        | r_h::r_t ->
+          let next_ertl = (pass_args (i + 1) r_t ertl) in
+          let next_lab = generate next_ertl in
+            if i < 6 then
+              Embinop (Mmov, r_h, List.nth Register.parameters i, next_lab)
+            else
+              Epush_param (r_h, next_lab)
+      in
+      pass_args 0 r_list ertl_call
   | Rtltree.Egoto l ->
       Ertltree.Egoto (l)
 
@@ -39,13 +59,51 @@ let rtl_to_ertl (l:Label.t) (i:Rtltree.instr) =
   graph := Label.M.add l einstr !graph
 
 let deffun (df:Rtltree.deffun) =
-  graph := Label.M.add df.fun_exit Ertltree.Ereturn Label.M.empty;
+  graph := Label.M.empty;
+  let l_ret = generate (Ertltree.Ereturn) in
+
+  (* delete frame *)
+  let r_callees = List.map (fun x -> Register.fresh ()) Register.callee_saved in
+  let l_del_frame = generate (Ertltree.Edelete_frame (l_ret)) in
+  let remove_callee reg r_pseudo l =
+    generate (Ertltree.Embinop(Mmov, r_pseudo, reg, l))
+  in
+  let l_rem_callee = List.fold_right2 remove_callee Register.callee_saved r_callees l_del_frame in
+
+  (* return value into rax *)
+  graph := Label.M.add df.fun_exit
+      (Ertltree.Embinop(Mmov, df.fun_result, Register.result, l_rem_callee)) !graph;
+
+  (* body instructions *)
   Label.M.iter rtl_to_ertl df.fun_body;
+
+  (* Get parameters from rdi,rsi etc*)
+  let rec recover_args i r_l l =
+    match r_l with
+    | [] -> l
+    | r_h::r_t ->
+      let l_next = (recover_args (i + 1) r_t l) in
+      generate (
+        if i < 6 then
+          Embinop (Mmov, List.nth Register.parameters i, r_h, l_next)
+        else
+          Eget_param (-8 * (i - 6), r_h, l)
+      )
+  in
+  let l_args = recover_args 0 df.fun_formals df.fun_entry in
+
+  (* alloc_frame *)
+  let save_callee reg r_pseudo l =
+    generate (Ertltree.Embinop(Mmov, reg, r_pseudo, l))
+  in
+  let l_callee = List.fold_right2 save_callee Register.callee_saved r_callees l_args in
+  let l_entry = generate (Ertltree.Ealloc_frame (l_callee)) in
+
   {
     fun_name    = df.fun_name;
-    fun_formals = Register.S.cardinal df.fun_locals;
-    fun_locals  = df.fun_locals;
-    fun_entry   = df.fun_entry;
+    fun_formals = List.length df.fun_formals;
+    fun_locals  = Register.S.union df.fun_locals (Register.set_of_list r_callees);
+    fun_entry   = l_entry;
     fun_body    = !graph;
   }
 
