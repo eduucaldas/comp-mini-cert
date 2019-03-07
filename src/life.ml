1c1cc8aa22ee5c93eb70f6ed6df333e002ad397a
Eduardo Caldas <eduucaldas@gmail.com>
2019-03-07 16:14:29 +0100

    better name life

--- src/life.ml
+++ src/life.ml
@@ -1,6 +1,7 @@
 open Format
 open Ertltree
 
+(* li abreviates live_info *)
 type live_info = {
           instr: Ertltree.instr;
           succ: Label.t list;    (* successeurs *)
@@ -11,10 +12,10 @@ type live_info = {
   mutable outs: Register.set;    (* variables vivantes en sortie *)
 }
 
-let add_live_info label_live l instr =
+let add_li l_to_li l instr =
   let succ = Ertltree.succ instr in
   let defs, uses = Ertltree.def_use instr in
-  let info = {
+  let li = {
     instr = instr;
     succ = succ;
     pred = Label.S.empty;
@@ -24,49 +25,49 @@ let add_live_info label_live l instr =
     outs = Register.S.empty;
   }
   in
-  Hashtbl.add label_live l info
+  Hashtbl.add l_to_li l li
 
-let set_pred_in_succs label_live l_pred inf_pred =
+let set_pred_in_succs l_to_li l_pred li_pred =
   let add_pred l_succ =
-    let inf_succ = Hashtbl.find label_live l_succ in
-    inf_succ.pred <- Label.S.add l_pred inf_succ.pred
+    let li_succ = Hashtbl.find l_to_li l_succ in
+    li_succ.pred <- Label.S.add l_pred li_succ.pred
   in
-  List.iter add_pred inf_pred.succ
+  List.iter add_pred li_pred.succ
 
-let kildall label_live =
+let kildall l_to_li =
   let ws = ref Label.S.empty in
-  Hashtbl.iter (fun l _ -> (ws := Label.S.add l !ws)) label_live;
+  Hashtbl.iter (fun l _ -> (ws := Label.S.add l !ws)) l_to_li;
   while not (Label.S.is_empty !ws) do
     let l = Label.S.choose !ws in
     ws := Label.S.remove l !ws;
-    let inf = Hashtbl.find label_live l in
-    let old_in = inf.ins in
+    let li = Hashtbl.find l_to_li l in
+    let in_old = li.ins in
     let get_in_from_label l =
-      (Hashtbl.find label_live l).ins
+      (Hashtbl.find l_to_li l).ins
     in
-    inf.outs <- List.fold_left (fun acc l -> Register.S.union (get_in_from_label l) acc)
-      Register.S.empty inf.succ;
-    inf.ins <- Register.S.union inf.uses (Register.S.diff inf.outs inf.defs);
-    if not (Register.S.equal inf.ins old_in) then
-      ws := Label.S.fold (fun l acc -> Label.S.add l acc) inf.pred !ws
+    li.outs <- List.fold_left (fun acc l -> Register.S.union (get_in_from_label l) acc)
+      Register.S.empty li.succ;
+    li.ins <- Register.S.union li.uses (Register.S.diff li.outs li.defs);
+    if not (Register.S.equal li.ins in_old) then
+      ws := Label.S.fold (fun l acc -> Label.S.add l acc) li.pred !ws
   done
 
 let liveness g =
-  let label_live = Hashtbl.create 255 in
-  Label.M.iter (add_live_info label_live) g;
-  Hashtbl.iter (set_pred_in_succs label_live) label_live;
-  kildall label_live;
-  label_live
+  let l_to_li = Hashtbl.create 255 in
+  Label.M.iter (add_li l_to_li) g;
+  Hashtbl.iter (set_pred_in_succs l_to_li) l_to_li;
+  kildall l_to_li;
+  l_to_li
 
 let print_set = Register.print_set
 
-let print_live_info fmt li =
+let print_li fmt li =
   Format.fprintf fmt "d={%a}@ u={%a}@ i={%a}@ o={%a}@\n"
     print_set li.defs print_set li.uses print_set li.ins print_set li.outs
 
 let print_graph_with_life fmt body entry =
-  let label_live = liveness body in
-Ertltree.visit (fun l i -> fprintf fmt "%a: %a " Label.print l print_instr i; print_live_info fmt (Hashtbl.find label_live l)) body entry
+  let l_to_li = liveness body in
+Ertltree.visit (fun l i -> fprintf fmt "%a: %a " Label.print l print_instr i; print_li fmt (Hashtbl.find l_to_li l)) body entry
 
 let print_with_liveness fmt p =
   fprintf fmt "=== ERTL + Liveness =======================================@\n";
