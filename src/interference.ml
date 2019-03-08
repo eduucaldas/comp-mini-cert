21e6f1629dca54b7e162bcb0c2657625691e1bd2
Eduardo Caldas <eduucaldas@gmail.com>
2019-03-08 09:56:51 +0100

    cleanup:interference graph

--- src/interference.ml
+++ src/interference.ml
@@ -5,40 +5,48 @@ type arcs = {
 
 type igraph = arcs Register.map
 
-let make live_info =
+let find_arcs g r =
+  match Register.M.find_opt r !g with
+  | Some a -> a
+  | None ->
+    let arc_new = {prefs = Register.S.empty; intfs = Register.S.empty} in
+    g := Register.M.add r arc_new !g;
+    arc_new
+
+let make l_to_li =
   let graph = ref Register.M.empty in
-  let find_arcs a =
-    if not (Register.M.mem a !graph) then
-      graph := Register.M.add a ({prefs = Register.S.empty; intfs = Register.S.empty}) !graph;
-    Register.M.find a !graph
-  in
-  let add_pref _ (live: Life.live_info) =
-    match live.instr with
-    | Ertltree.Embinop (Mmov, x, y, _) when x != y ->
-      let arcs_x = find_arcs x in
-      let arcs_y = find_arcs y in
-      arcs_x.prefs <- Register.S.add y arcs_x.prefs;
-      arcs_y.prefs <- Register.S.add x arcs_y.prefs
+
+  let add_pref _ (li: Life.live_info) =
+    match li.instr with
+    | Ertltree.Embinop (Mmov, r_1, r_2, _) when r_1 != r_2 ->
+      let arcs_1 = find_arcs graph r_1 in
+      let arcs_2 = find_arcs graph r_2 in
+      arcs_1.prefs <- Register.S.add r_2 arcs_1.prefs;
+      arcs_2.prefs <- Register.S.add r_1 arcs_2.prefs
     | _ -> ()
   in
-  Hashtbl.iter add_pref live_info;
-  let add_label _ (live: Life.live_info) = 
-    let add_def r_def = 
+  Hashtbl.iter add_pref l_to_li;
+
+  let add_label _ (li: Life.live_info) =
+    let add_def r_def =
       let add_intf r_alive =
-        match live.instr with
-        | Ertltree.Embinop (Mmov, x, y, _) when x == r_alive && y == r_def -> ()
+        match li.instr with
+        | Ertltree.Embinop (Mmov, r_1, r_2, _) when r_1 == r_alive && r_2 == r_def -> ()
         | _ ->
           if r_alive != r_def then
-            let arcs_def = find_arcs r_def in
-            let arcs_alive = find_arcs r_alive in
+            let arcs_def = find_arcs graph r_def in
+            let arcs_alive = find_arcs graph r_alive in
             arcs_alive.intfs <- Register.S.add r_def arcs_alive.intfs;
-            arcs_def.intfs <- Register.S.add r_alive arcs_def.intfs
+            arcs_def.intfs <- Register.S.add r_alive arcs_def.intfs;
+            if Register.S.mem r_alive arcs_def.prefs then
+              arcs_alive.prefs <- Register.S.remove r_def arcs_alive.prefs;
+              arcs_def.prefs <- Register.S.remove r_alive arcs_def.prefs
       in
-      Register.S.iter add_intf live.outs
+      Register.S.iter add_intf li.outs
     in
-    Register.S.iter add_def live.defs
+    Register.S.iter add_def li.defs
   in
-  Hashtbl.iter add_label live_info;
+  Hashtbl.iter add_label l_to_li;
   !graph
 
 let print ig =
@@ -46,6 +54,6 @@ let print ig =
       Format.printf "%s: prefs=@[%a@] intfs=@[%a@]@." (r :> string)
         Register.print_set arcs.prefs Register.print_set arcs.intfs) ig
 
-let print_file fmt (p: Ertltree.file) = 
+let print_file fmt (p: Ertltree.file) =
   Format.fprintf fmt "=== Interference Graph =================================================@\n";
   List.iter (fun (f: Ertltree.deffun) -> print(make (Life.liveness f.fun_body))) p.funs
